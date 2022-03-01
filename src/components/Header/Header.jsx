5fa4c5dea4aafde7a6e39c9ba74a158f26243279
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-03-01 21:10:12 +0100

    Create Login form and try out images

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Header() {
   return (
       <header>
-          <nav>
+          {/* <nav>
               <a href='#'>Home</a>
               <a href='#'>Characters</a>
               <a href='#'>Films</a>
               <a href='#'>Contact</a>
-          </nav>
+          </nav> */}
           <img src='https://www.pngkit.com/png/full/432-4327979_disfruta-de-los-20-renders-de-la-pelicula.png' alt='logo'/>
       </header>
   )
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Button from '../../components/Button/Button';
+import anakin from './assets/images/anakinLightSaber.png';
+import obiWan from './assets/images/obiWanLightSaber.png';
+import anonimus from './assets/images/anonimusLightSaber.png';
 
 export default function Login() {
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
   return (
     <div>
-        <h1>Welcome young padawan, identify yourself</h1>
+        <h1>Welcome, young padawan. 
+        It is time to identify yourself:</h1>
+
+        <img src={anakin} alt='imageLightSaberOff'/>
+
         <form>
             <label for="userName">User Name</label>
-            <input type="text" id="userName" name="userName" value="userName"/>
+            <input type="text" id="userName" 
+            name="userName" value={userName} 
+            onChange={e => setUserName(e.target.value)}/>
 
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" value="email"/>
+            <input type="email" id="email" 
+            name="email" value={email} 
+            onChange={e => setEmail(e.target.value)}/>
 
             <label for="password">Password</label>
-            <input type="password" id="password" name="password" value="password"/>
-            <button>Login</button>
+            <input type="password" id="password" 
+            name="password" value={password} 
+            onChange={e => setPassword(e.target.value)}/>
+
+            <Button btnContain='Login'/>
         </form>
+
+        <img src={obiWan} alt='imageLightSaberOff'/>
+
+        <img src={anonimus} alt='imageLightSaberOff'/>
+
     </div>
   )
 }
