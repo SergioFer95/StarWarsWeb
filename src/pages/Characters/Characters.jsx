39f9b9e7146408c0dccd63d60d06d9d71fba5d09
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2022-03-04 21:30:30 +0100

    Add some styñes and try out flex

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Characters() {
   return (
     <section>
-        <h2>CHARACTERS</h2>
+        <h2>Characters</h2>
     </section>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className="homeBody">Home</div>
+    <div className="homeBody">
+      <h2>Home</h2>
+    </div>
   )
 }
